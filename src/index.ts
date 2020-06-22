0a42e5345da7b846b7a421bf9ef78158b94aa56d
Eric M. Dantas <ericdantas0@gmail.com>
2020-06-22 16:14:08 -0300

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { MyInterface } from "typings-package1-issue" 
 
 export class ClassFromPackage2 implements MyInterface {
+    name = "package2";
     doSomething():string {
-        return "hello from package2!";
+        return `hello from ${this.name}!`;
     }
 }
 
